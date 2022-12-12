9eb851693fff0570c49341ddb09c3547630f2e74
fearless <983527455@qq.com>
2022-12-12 15:51:57 +0800

    fix:判断重复命令

--- src/Service/Service.php
+++ src/Service/Service.php
@@ -8,13 +8,14 @@ use Fearless\ThinkTool\Command\DevAndLogCommand;
 use Fearless\ThinkTool\Command\MakeController;
 use Fearless\ThinkTool\Command\TestCommand;
 use Fearless\ThinkTool\Command\UpdateMenu;
+use think\facade\Console;
 
 class Service extends \think\Service
 {
     public function boot()
     {
         $commands = [TestCommand::class,MakeController::class,UpdateMenu::class,DevAndLogCommand::class,'hw:cover'=>DevAndLogCommand::class];
-        if (!is_object(Install::class)){
+        if (!isset(Console::all()['hw:addons'])){
             $commands[] = Install::class;
         }
         $this->commands($commands);
