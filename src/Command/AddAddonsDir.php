666b30a4b65ca4df049eecdffb11be53d2db98d7
fearless <983527455@qq.com>
2022-12-23 09:40:23 +0800

    fix:更新插件

--- src/Command/AddAddonsDir.php
+++ src/Command/AddAddonsDir.php
@@ -18,10 +18,11 @@ class AddAddonsDir extends \think\console\Command
     public function execute(Input $input, Output $output)
     {
         if (!is_dir(root_path('addons'))){
-            copyDir(__DIR__.DIRECTORY_SEPARATOR.'..'
-                .DIRECTORY_SEPARATOR.'Files'.DIRECTORY_SEPARATOR
-                .'addons_dir', root_path('addons'));
+            mkdir(root_path('addons'));
         }
+        copyDir(__DIR__.DIRECTORY_SEPARATOR.'..'
+            .DIRECTORY_SEPARATOR.'Files'.DIRECTORY_SEPARATOR
+            .'addons_dir', root_path('addons'));
         $output->writeln('addons ready');
     }
 }
